8f30ae3916cf573c54088041dddcc3c834970ff8
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-01 22:00:59 +0700

    menambah file seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 6),
+            'product_name' => $this->faker->bothify('???-#?##'),
+            'price' => $this->faker->numberBetween(23_000, 940_000),
+            'image' => 'https://picsum.photos/450/300?random=' . $this->faker->numberBetween(1, 100),
+            'description' => $this->faker->paragraph(1),
+        ];
+    }
+}
