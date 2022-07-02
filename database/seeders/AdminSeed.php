2932c4fafc73805c788c61accf298a1d486bbe00
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-07-02 10:33:17 +0700

    penambahan file AdmiSeed

---
+++ database/seeders/AdminSeed.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class AdminSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'username' => 'admin1',
+            'phone' => '085270430926',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
